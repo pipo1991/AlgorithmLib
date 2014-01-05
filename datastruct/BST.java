cd029049a58e73098d498289ca87e958a8b74d99
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2014-01-05 13:48:48 +0200

    Update BST.java

--- datastruct/BST.java
+++ datastruct/BST.java
@@ -1,57 +1,68 @@
 //Manipulate class for BST
-class BSTree{
+class BSTree {
+
     private BST tree;
 
     public BSTree() {
-        tree=null;
+        tree = null;
     }
-    
-    void add(int x){
-        if(tree==null){
-            tree=new BST(x);
-        }else{
+
+    void add(int x) {
+        if (tree == null) {
+            tree = new BST(x);
+        } else {
             BST.insertNode(tree, x);
         }
     }
-    
-    void delete(int x){
-        if(tree!=null){
-            boolean flag=tree.isOnlyRoot();
-            int returnVal=BST.deleteNode(tree, x);
-            if(flag && returnVal==1){
-                tree=null;
+
+    void delete(int x) {
+        if (tree != null) {
+            boolean flag = tree.isOnlyRoot();
+            int returnVal = BST.deleteNode(tree, x);
+            if (flag && returnVal == 1) {
+                tree = null;
             }
-        }else{
+        } else {
             System.out.println("Warning !! Deleting from an empty tree.");
         }
     }
-    
-    int search(int x){
+
+    int search(int x) {
         return BST.search(tree, x);
     }
-    
-    int getMax(){
+
+    int getMax() {
         return BST.getMax(tree);
     }
-    
-    int getMin(){
+
+    int getMin() {
         return BST.getMin(tree);
     }
-    
-    int getDepth(){
+
+    int getDepth() {
         return BST.getMaxDepth(tree);
     }
 }
 // simple implemantation for simple unbalnced BST
-class BST{
+class BST {
+
+    private BST parent;
     private BST right;
     private BST left;
     int data;
-    
-    BST(int x){
-        data=x;
-        right=null;
-        left=null;
+
+    BST(int x) {
+        data = x;
+        right = null;
+        left = null;
+        parent = null;
+    }
+
+    BST(int x, BST p) {
+        data = x;
+        right = null;
+        left = null;
+        parent = p;
     }
 
     public int getData() {
@@ -77,87 +88,155 @@ class BST{
     public void setRight(BST right) {
         this.right = right;
     }
-   
+
+    public BST getParent() {
+        return parent;
+    }
+
+    public void setParent(BST parent) {
+        this.parent = parent;
+    }
+
     //Using Recursion
-    static void insertNode(BST root, int x){
-        if(root.getData()==x)
+    static void insertNode(BST root, int x) {
+        if (root.getData() == x) {
             return;
-        if(x<root.getData()){
-            if(root.getLeft()==null)
-                root.setLeft(new BST(x));
-            else
-              insertNode(root.getLeft(),x);
         }
-        else{
-            if(root.getRight()==null)
-                root.setRight(new BST(x));
-            else
-              insertNode(root.getRight(),x);
+        if (x < root.getData()) {
+            if (root.getLeft() == null) {
+                root.setLeft(new BST(x, root));
+            } else {
+                insertNode(root.getLeft(), x);
+            }
+        } else {
+            if (root.getRight() == null) {
+                root.setRight(new BST(x, root));
+            } else {
+                insertNode(root.getRight(), x);
+            }
         }
     }
-   
-    
-    static int deleteNode(BST root, int x){
-        BST parent=root;
-        BST temp=root;
-       while(temp!=null && temp.getData()!=x){
-           parent=temp;
-           if(x<temp.getData()){
-               temp=temp.getLeft();
-           }else{
-               temp=temp.getRight();
-           }
-       }
-       if(temp==null){
-           //Not Found
-           return -1;
-       }
-       while(true){
-           /*
-            *  To be implemented ... 
-            *  Removing the node 
-            */
-       }
-       //Deleted
-       return 1;
+
+    static int deleteNode(BST root, int x) {
+        BST parentTemp = null;
+        BST temp = root;
+        while (temp != null && temp.getData() != x) {
+            parentTemp = temp;
+            if (x < temp.getData()) {
+                temp = temp.getLeft();
+            } else {
+                temp = temp.getRight();
+            }
+        }
+        if (temp == null) {
+            //Not Found
+            return -1;
+        }
+        if (temp.getRight() == null && temp.getLeft() == null) {
+            if(parentTemp !=null){
+                if (parentTemp.getRight() != null && parentTemp.getRight().getData() == temp.getData()) {
+                    parentTemp.setRight(null);
+                } else {
+                    parentTemp.setLeft(null);
+                }
+            }
+        } else if (temp.getLeft() == null) {
+            if(parentTemp !=null){
+                if (parentTemp.getRight() != null && parentTemp.getRight().getData() == temp.getData()) {
+                    parentTemp.setRight(temp.getRight());
+                } else {
+                    parentTemp.setLeft(temp.getRight());
+                }
+            }else{
+               temp.setData(temp.getRight().getData());
+               temp.setRight(null);
+            }
+        } else if (temp.getRight() == null) {
+            if(parentTemp !=null){
+                if (parentTemp.getRight().getData() == temp.getData()) {
+                    parentTemp.setRight(temp.getLeft());
+                } else {
+                    parentTemp.setLeft(temp.getLeft());
+                }
+            }else{
+               temp.setData(temp.getLeft().getData());
+               temp.setLeft(null);
+            }
+        } else {
+            int data = deleteMin(temp.getRight());
+            temp.setData(data);
+        }
+        //Deleted
+        return 1;
     }
-    
-    public boolean isOnlyRoot(){
-        return this.left==null && this.right==null;
+
+    public boolean isOnlyRoot() {
+        return this.left == null && this.right == null;
     }
-            
-    static int search(BST root, int x){
-        if(root== null)
+
+    static int search(BST root, int x) {
+        if (root == null) {
             return 0;
-        if(root.getData()==x){
+        }
+        if (root.getData() == x) {
             return 1;
         }
-        if(x<root.getData()){
+        if (x < root.getData()) {
             return search(root.getLeft(), x);
         }
         return search(root.getRight(), x);
     }
-    
-    
-    static int getMaxDepth(BST bst){
-        if(bst==null)
+
+    static int getMaxDepth(BST bst) {
+        if (bst == null) {
             return 0;
-        return Math.max(getMaxDepth(bst.getLeft()),getMaxDepth(bst.getRight()))+1;
+        }
+        return Math.max(getMaxDepth(bst.getLeft()), getMaxDepth(bst.getRight())) + 1;
     }
-    
-    static int getMin(BST bst){
-        BST temp=bst;
-        while(temp.getLeft()!=null){
-            temp=temp.getLeft();
+
+    static int getMin(BST bst) {
+        BST temp = bst;
+        while (temp.getLeft() != null) {
+            temp = temp.getLeft();
         }
         return temp.getData();
     }
-    
-    static int getMax(BST bst){
-        BST temp=bst;
-        while(temp.getRight()!=null){
-            temp=temp.getRight();
+
+    static int getMax(BST bst) {
+        BST temp = bst;
+        while (temp.getRight() != null) {
+            temp = temp.getRight();
         }
         return temp.getData();
+    }
+
+    static int deleteMax(BST bst) {
+        BST temp = bst;
+        while (temp.getRight() != null) {
+            temp = temp.getRight();
+        }
+        temp = temp.getParent();
+        int data = temp.getData();
+        if (temp.getLeft().getData() == data) {
+            temp.setLeft(null);
+        } else {
+            temp.setRight(null);
+        }
+        return data;
+    }
+
+    static int deleteMin(BST bst) {
+        BST temp = bst;
+        while (temp.getLeft() != null) {
+            temp = temp.getLeft();
+        }
+        int data = temp.getData();
+        temp = temp.getParent();
+        if (temp.getLeft().getData() == data) {
+            temp.setLeft(null);
+        } else {
+            temp.setRight(null);
+        }
+        return data;
     }
 }
